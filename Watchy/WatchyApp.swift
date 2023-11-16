86c5a6d717cbeae7021ee8a50c675fa9767de739
olexey27 <allxyog@gmail.com>
2023-11-16 02:57:29 +0100

    Initial Commit

---
+++ Watchy/WatchyApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchyApp.swift
+//  Watchy
+//
+//  Created by Alexej K on 16.11.23.
+//
+
+import SwiftUI
+
+@main
+struct WatchyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
